2d2f6eb4de6aa8b3392be4354d7cc1b78fa9f432
Ashwani333 <56716113+Ashwani333@users.noreply.github.com>
2019-10-21 23:45:22 +0530

    Update simulator.js

--- experiment/simulation/js/simulator.js
+++ experiment/simulation/js/simulator.js
@@ -1,89 +1,71 @@
 (function() {
   const myQuestions = [
+
     {
-      question: "1.The circuit used for producing AM is called?",
+
+      question: "1.Titration is used to find the strength of ....... solution by using known solution?",
+
       answers: {
-        a: "Modulator",
-        b: "Transmitter",
-        c: "Receiver",
-        d:" Duplexer"
+
+        a: " Liquid Solution",
+
+        b: " Fluid Solution",
+
+        c: " Buffer solution",
+
+        d: " Unknown Solution ",
+
       },
-      correctAnswer: "a"
-    },
-    {
-      question: "2.In Amplitude Modulation, the instantaneous values of the carrier amplitude changes in accordance with the amplitude and frequency variations of the modulating signal.",
-      answers: {
-        a: "True",
-        b: "False",
-       
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "3.What is the line connecting the positive and negative peaks of the carrier waveform called? ",
-      answers: {
-        a: "Peak line",
-        b: "Maximum amplitude ceiling",
-        c: "Modulation index",
-        d: "Envelope"
-    },
+
       correctAnswer: "d"
-    },  
-    {
-      question: "4.The ratio between the modulating signal voltage and the carrier voltage is called? ",
-      answers: {
-        a: "Amplitude modulation",
-        b: "Modulation frequency",
-        c: "Modulation index",
-        d: "Ratio of modulation"
-      },
-      correctAnswer: "c" 
+
     },
+
     {
-      question: "5.What happens when the amplitude of the modulating signal is greater than the amplitude of the carrier?",
+
+      question: "2.KMnO4 is ........?",
+
       answers: {
-        a: "Decay ",
-        b: "Distortion",
-        c: "Amplification",
-        d: "Attenuation"
-    },
-      correctAnswer: "b"
-    },  
-    {
-      question: "6.What is the condition for greatest output power at the transmitter without distortion? ",
-      answers: {
-        a: "Modulating signal voltage > Carrier voltage",
-        b: "Modulating signal voltage < Carrier voltage",
-        c: "Modulating signal voltage = Carrier voltage",
-        d: "Modulating signal voltage = 0"
+
+        a: " Acid",
+
+        b: " Base",
+        
+        c: " Self Indicator",
+          
+        d: " None Of These"
       },
-      correctAnswer: "c" 
+        
+        
+
+
+
+      correctAnswer: "c"
+
     },
-     
+
     {
-      question: "7.What is the bandwidth of the signal if the highest frequency in the modulating signal is 3 KHz?",
+
+      question: "3.What is the formula of oxalic acid ? ",
+
       answers: {
-        a: "6Khz",
-        b: "3KHz",
-        c: "5KHz",
-        d: "140KHz"
+
+        a: "HCOOH",
+
+        b: "CH3COOH ",
+
+        c: "C2H5OH",
+
+        d: " C2H2O4 "
+
       },
-      correctAnswer: "a" 
+
+      correctAnswer: "d"
+
     },
-    {
-      question: "8.What is the wave of frequency that occurs at the sum and difference of the carrier and modulating signal is called? ",
-      answers: {
-        a: "Noise signals ",
-        b: "Sideband",
-        c: "Extraband",
-        d: "Neutral band"
-      },
-      correctAnswer: "b" 
-       
-     
-     
-    }
-    
+
+   
+
   ];
 
   function buildQuiz() {
